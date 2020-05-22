890641d7743f9327e1ba3ae5b299d8fdba3ecedb
bharat_patil <patilbharat09@gmail.com>
2020-05-22 15:56:35 +0530

    Added cross to allow cross origin

--- server/app.js
+++ server/app.js
@@ -2,8 +2,13 @@ const express = require('express');
 const graphqlHTTP = require('express-graphql');
 const schema = require('./schema/schema');
 const mongoose = require('mongoose')
+const cors = require('cors');
 
 const app = express();
+
+//allow cross origin
+app.use(cors());
+
 const dbURL = process.env.GRAPHQL_MONGODB_URL;
 mongoose.connect(dbURL, { useNewUrlParser: true });
 mongoose.connection.once('open',() => {
